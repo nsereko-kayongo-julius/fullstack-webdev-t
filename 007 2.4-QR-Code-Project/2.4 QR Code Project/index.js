bdd03d086b932d99d8cb956432366d5a03511b54
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2024-01-03 00:13:42 +0300

    initialize Npm in a project and learn about it

---
+++ 007 2.4-QR-Code-Project/2.4 QR Code Project/index.js
@@ -0,0 +1,34 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      message: "waht is your link: ",
+      name: "url",
+    },
+  ])
+  .then((answers) => {
+    const URL = answers.url;
+    var qr_svg = qr.image(URL);
+    qr_svg.pipe(fs.createWriteStream("qr4_img.png"));
+
+    fs.writeFile("url.txt", URL, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
